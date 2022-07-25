d117c5b444a7ea6e2d5a56e5f609b4e73652f9a5
bschonberger <ben.schonberger1@gmail.com>
2022-07-24 22:32:15 -0400

    .DS_Store

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
-// Function to hide start div and show end div
-let startContainer = document.getElementById('startcontainer');
-let btn = document.querySelector("#startbutton");
 
-btn.addEventListener('click', function(){
-    gameButtons = document.querySelectorAll(".gameselection").forEach(item => {
-        item.addEventListener("click", playSound);
-    })
-});
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+Rock.addEventListener('click', () => handleClick('Rock'))
+Paper.addEventListener('click', () => handleClick('Paper'))
+Scissors.addEventListener('click', () => handleClick('Scissors'))
+
+function handleClick(playerSelection) {
+
+    const computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection)
+  }
 
 function computerPlay () {
-    const choices = ["Rock", "Paper","Scissors"]
-    return choices[getRandomInt(3)]
+    const choices = ["Rock", "Paper","Scissors"];
+    return choices[getRandomInt(3)];
     
 }
 function getRandomInt(max) {
@@ -19,86 +23,140 @@ function getRandomInt(max) {
 
 /* This Functions Returns a 0 if it is a tie, a 1 if the player wins, and a 2 if the computer wins. */
 function playRound(playerSelection,computerSelection){
-
-    console.log("Player Selection is: " + playerSelection)
-    console.log("Computer Selection is: " + computerSelection)
-
-    playerSelection = playerSelection.toLowerCase(playerSelection)
-
-    if (playerSelection == "rock"){
+ 
+    if (playerSelection == "Rock"){
 
         if (computerSelection == "Rock"){
-            console.log("It's a Tie!")
-            return(0)
+            roundWinner = "Tie";
             }
         if (computerSelection == "Paper"){
-            console.log("Computer Wins!")
-            return(2)
+            computerScore++;
+            roundWinner = "Computer";
             }
         if (computerSelection == "Scissors"){
-            console.log("Player Wins!")
-            return(1)
+            playerScore++;
+            roundWinner = "Player";
             }
         }
-    else if (playerSelection == "paper"){
+    else if (playerSelection == "Paper"){
 
         if (computerSelection == "Rock"){
-            console.log("Player Wins!")
-            return(1)
+            playerScore++;
+            roundWinner = "Player";
             }
         if (computerSelection == "Paper"){
-            console.log("It's a Tie!")
-            return(0)
+            roundWinner = "Tie";
             }
         if (computerSelection == "Scissors"){
-            console.log("Computer Wins!")
-            return(2)
+            computerScore++;
+            roundWinner = "Computer";
             }
         }
-    else if (playerSelection == "scissors"){
+    else if (playerSelection == "Scissors"){
 
         if (computerSelection == "Rock"){
-            console.log("Computer Wins!")
-            return(2)
+            computerScore++;
+            roundWinner = "Computer";
             }
         if (computerSelection == "Paper"){
-            console.log("Player Wins!")
-            return(1)
+            playerScore++;
+            roundWinner = "Player";
             }
         if (computerSelection == "Scissors"){
-            console.log("It's a Tie!")
-            return(0)
+            roundWinner = "Tie";
             }
         }
+        
+        updateScore(roundWinner,playerScore,computerScore);
+        updateReplay(playerSelection,computerSelection);
  
 }
-  
-function game(playRound){
-    let playerWins = 0;
-    let computerWins = 0;
+function updateScore(roundWinner,playerScore,computerScore){
 
-    while ((playerWins < 3) && (computerWins < 3)) {
-
-
-        const playerSelection = prompt("Rock, Paper, or Scissors?");
-        const computerSelection = computerPlay();
-        winner = playRound(playerSelection,computerSelection);
-
-        if (winner==0){
-            console.log("Player wins: " + playerWins)
-            console.log("Computer Wins: " + computerWins)
-            }
-        if (winner==1){
-            playerWins++;
-            console.log("Player wins: " + playerWins)
-            console.log("Computer Wins: " + computerWins)
-            }
-        if (winner==2){
-            computerWins++;
-            console.log("Player wins: " + playerWins)
-            console.log("Computer Wins: " + computerWins)
-            }
+    const container = document.querySelector('#winner');
+    const container2 = document.querySelector('#score');
+    if (roundWinner == "Player"){
+        container.innerHTML = "Player wins this round.";
+        container2.innerHTML = "Score: Player " + playerScore + " Computer " + computerScore
+    }
+    if (roundWinner == "Computer"){
+        container.innerHTML = "Computer wins this round.";
+        container2.innerHTML = "Score: Player " + playerScore + " Computer " + computerScore
+    }    
+    if (roundWinner == "Tie"){
+        container.innerHTML = "Tie.";
+        container2.innerHTML = "Score: Player " + playerScore + " Computer " + computerScore
+    } 
+    
+    if ((playerScore == 3) || (computerScore == 3)){
+        let winner;
+        if (playerScore == 3){
+            console.log("You Won!");
+            winner = "Player";
         }
+        else{
+            console.log("You Lost...")
+            winner = "Computer";
+        }
+        container.innerHTML = "";
+        container2.innerHTML = "Score: ";
+        endGame(winner);
+    }
+}
+
+function updateReplay(playerSelection,computerSelection){
+    const playerIcon = document.querySelector('#playerIcon')
+    const computerIcon = document.querySelector('#computerIcon')
+
+    if (playerSelection == "Rock"){
+        playerIcon.src = "./images/rock.png";
+    } 
+    if (playerSelection == "Paper"){
+        playerIcon.src = "./images/paper.png";
+    }   
+    if (playerSelection == "Scissors"){
+        playerIcon.src = "./images/scissors.png";
+    }  
+    if (computerSelection == "Rock"){
+        computerIcon.src = "./images/rock.png";
+    }  
+    if (computerSelection == "Paper"){
+        computerIcon.src = "./images/paper.png";
+    }  
+    if (computerSelection == "Scissors"){
+        computerIcon.src = "./images/scissors.png";
     }  
 
-/*game(playRound) */
+}
+
+function endGame(winner){
+
+    let modal = document.getElementById("myModal");
+    let gameWinner = document.querySelector("p");
+    if (winner == "Player"){
+        gameWinner.innerHTML = "You Won!";
+    }
+    else {
+        gameWinner.innerHTML = "You Lost...";
+    }
+
+    let span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+        computerScore = 0;
+        playerScore = 0;
+        playerIcon.src = "./images/question.png";
+        computerIcon.src = "./images/question.png";
+
+
+
+}
+
+}
+  
+
+
+
+
